fe7ccfdadd4fa02198da82493d94216ea94a67c5
57xiaoc <2415777318@qq.com>
2022-09-22 23:52:40 +0800

    2022/09/22

---
+++ P1428 小鱼比可爱/main.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int N = 110;
+int a[N];
+
+int main(){
+    
+    // freopen("../in.txt","r",stdin);   //从文件mid.in里读取数据
+    // freopen("../out.txt","w",stdout);   //程序运行会创建文件mid.out将结果写入
+    int n;
+    cin>>n;
+    for(int i = 1;i<=n;i++) cin>>a[i];
+    cout<<0<<' ';
+    int i = 2;
+    while(i<=n){
+        int ans = 0;
+        for(int j = i-1;j!=0;j--) if(a[j]<a[i]) ans ++;
+        cout<<ans<<' ';
+        i++;
+    }
+    return 0;
+}
